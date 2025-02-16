7ef89faa749ec46ab62c749654a299ab222bb916
myka0 <mykamayer0@gmail.com>
2025-02-16 16:10:06 -0600

    Generation 514 2025-02-16 16:10:00

--- modules/user/git.nix
+++ modules/user/git.nix
@@ -1,11 +1,13 @@
-{
+{pkgs, ...}: {
   programs.git = {
     enable = true;
     userName = "myka0";
     userEmail = "mykamayer0@gmail.com";
     extraConfig = {
       init.defaultBranch = "main";
-      credential.helper = "libsecret";
+      credential.helper = "${
+        pkgs.git.override {withLibsecret = true;}
+      }/bin/git-credential-libsecret";
     };
   };
 }
