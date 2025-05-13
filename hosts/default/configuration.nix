b4d4e3e2b28f2418554fc11f7431b8d8d935e073
myka0 <mykamayer0@gmail.com>
2025-05-13 16:23:40 -0400

    Generation 547 2025-05-13 16:23:35

--- hosts/default/configuration.nix
+++ hosts/default/configuration.nix
@@ -57,7 +57,7 @@
   programs.nix-ld.enable = true;
 
   # Set time zone
-  time.timeZone = "America/Chicago";
+  time.timeZone = "America/New_York";
 
   # Select internationalisation properties
   i18n.defaultLocale = "en_US.UTF-8";
