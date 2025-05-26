568c76a9fd0b98d051ce13358f52dbc2636e7db1
myka0 <mykamayer0@gmail.com>
2025-05-25 21:43:44 -0400

    Generation 553 2025-05-25 21:41:37

--- modules/system/sound.nix
+++ modules/system/sound.nix
@@ -17,7 +17,7 @@
     loopback.enable = false;
   };
 
-  hardware.pulseaudio.enable = false;
+  services.pulseaudio.enable = false;
 
   environment.systemPackages = with pkgs; [pavucontrol paprefs];
 }
