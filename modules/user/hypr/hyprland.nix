1c68e10aa93f01210186c167564618c09ce6e78b
myka0 <mykamayer0@gmail.com>
2026-04-20 20:01:56 -0500

    Generation 684 2026-04-20 20:01:52

--- modules/user/hypr/hyprland.nix
+++ modules/user/hypr/hyprland.nix
@@ -121,7 +121,7 @@ in {
       };
 
       extraConfig = ''
-        monitor = eDP-1, preferred, auto, 1
+        monitor = eDP-1, preferred, 0x0, 1
         monitor = HDMI-A-1, preferred, 2560x0, 1
         monitor = DP-1, 1920x1080@144, 4480x0, 1
         monitor = DP-3, preferred, auto, 1
