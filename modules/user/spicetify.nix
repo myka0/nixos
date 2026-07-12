06b13e6ccfbe1b06c83e7e3086123a3a2ae627a4
myka0 <mykamayer0@gmail.com>
2026-07-12 11:45:27 -0500

    Generation 714 2026-07-12 11:42:59

--- modules/user/spicetify.nix
+++ modules/user/spicetify.nix
@@ -24,9 +24,17 @@
       ncsVisualizer
       historyInSidebar
       marketplace
+      {
+        src = pkgs.fetchFromGitHub {
+          owner = "Xndr2";
+          repo = "listening-stats";
+          rev = "5507c91120ae7f06b702ff4ef4f55df058770527";
+          hash = "sha256-sChfQWrTuZ3TgPvstNSOh1ytOtU0Iel29S1uXLilKzU=";
+        };
+        name = "listening-stats";
+      }
     ];
     enabledSnippets = with spicePkgs.snippets; [
-      rotatingCoverart
       pointer
     ];
   };
