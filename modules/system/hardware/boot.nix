b250b34744043a999355116d5d7a541324c43edf
myka0 <mykamayer0@gmail.com>
2026-07-18 11:52:26 -0500

    Generation 718 2026-07-18 11:48:35

--- modules/system/hardware/boot.nix
+++ modules/system/hardware/boot.nix
@@ -16,31 +16,7 @@ in {
       "atkbd.soft_raw=1"
     ];
 
-    kernelPackages = pkgs.linuxPackages_7_0;
-    kernelPatches = map (patch: {inherit patch;}) [
-      # asus-armoury model support patches
-      "${g14_patches}/0003-platform-x86-asus-armoury-add-support-for-FA507UV.patch"
-      "${g14_patches}/0003-platform-x86-asus-armoury-add-support-for-FA608UM.patch"
-      "${g14_patches}/0003-platform-x86-asus-armoury-add-support-for-G615LR.patch"
-      "${g14_patches}/0003-platform-x86-asus-armoury-add-support-for-G835LW.patch"
-      "${g14_patches}/0003-platform-x86-asus-armoury-add-support-for-GA403WR.patch"
-      "${g14_patches}/0003-0-4-platform-x86-asus-armoury-ppt-fixes-and-new-models.patch"
-      "${g14_patches}/0001-platform-x86-asus-armoury-add-support-for-FX608JMR.patch"
-
-      # ACPI / power management patches
-      "${g14_patches}/0001-acpi-proc-idle-skip-dummy-wait.patch"
-      "${g14_patches}/0070-acpi-x86-s2idle-Add-ability-to-configure-wakeup-by-A.patch"
-
-      # audio fixes
-      "${g14_patches}/ga403wr-fix-audio.patch"
-
-      # battery patches
-      "${g14_patches}/0001-platform-asus-wmi-do-not-enforce-battery.patch"
-      "${g14_patches}/0002-g614fp.patch"
-
-      # lamparray support
-      "${g14_patches}/v4-0001-lamparray.patch"
-    ];
+    kernelPackages = pkgs.linuxPackages_latest;
 
     loader = {
       efi = {
